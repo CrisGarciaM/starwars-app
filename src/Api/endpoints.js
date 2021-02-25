4783782e4e8a4d69eac1e9e25183a564a25964d7
CristoferGarcia1984 <60959942+CristoferGarcia1984@users.noreply.github.com>
2021-02-25 17:44:32 +0100

    Add api main endpoints

---
+++ src/Api/endpoints.js
@@ -0,0 +1,8 @@
+const BASE_URL = 'https://swapi.dev/api/';
+
+export const mainEndPoints = {
+  characters: BASE_URL + 'people/',
+  species: BASE_URL + 'species/',
+  planets: BASE_URL + 'planets/',
+  startShips: BASE_URL + 'starships/',
+};
