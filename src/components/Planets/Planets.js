fb6c618fc682f2de4f0bfe23ddf5da43ff3de057
CristoferGarcia1984 <60959942+CristoferGarcia1984@users.noreply.github.com>
2021-02-26 11:49:40 +0100

    Create initial planets rendering

---
+++ src/components/Planets/Planets.js
@@ -0,0 +1,57 @@
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import PlanetsCard from './PlanetsCard/PlanetsCard';
+import ParentContainer from '../ParentContainer/ParentContainer';
+import { fetchPlanets } from '../../redux/planets/planets-actions';
+import { mainEndPoints } from '../../Api/endpoints';
+
+const Planets = (props) => {
+  const { fetchPlanets, planetsData } = props;
+  const { planets, loading } = planetsData;
+
+  useEffect(() => {
+    fetchPlanets(mainEndPoints.planets);
+  }, [fetchPlanets]);
+
+  if (loading) {
+    return <div className="w-100 text-center">Loading...</div>;
+  }
+
+  return (
+    <div>
+      <ParentContainer>
+        {planets.results.map((singlePlanet, index) => {
+          return (
+            <PlanetsCard
+              key={index}
+              id={singlePlanet.id}
+              name={singlePlanet.name}
+              terrain={singlePlanet.terrain}
+              rotationPeriod={singlePlanet.rotation_period}
+              orbitalPeriod={singlePlanet.orbital_period}
+              diameter={singlePlanet.diameter}
+              climate={singlePlanet.climate}
+              gravity={singlePlanet.gravity}
+              surfaceWater={singlePlanet.surface_water}
+              population={singlePlanet.population}
+            />
+          );
+        })}
+      </ParentContainer>
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => {
+  return {
+    planetsData: state.planetsState,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    fetchPlanets: (endpoint) => dispatch(fetchPlanets(endpoint)),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Planets);
