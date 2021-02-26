a05fbdaec7ec3932a6288c0bec9d139200486684
CristoferGarcia1984 <60959942+CristoferGarcia1984@users.noreply.github.com>
2021-02-26 13:53:30 +0100

    Add starships reducer

--- src/root-reducer.js
+++ src/root-reducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import CharactersReducer from './redux/characters/characters-reducer';
 import PlanetsReducer from './redux/planets/planets-reducer';
+import StartShipsReducer from './redux/starships/starships-reducer';
 
 const rootReducer = combineReducers({
   charactersState: CharactersReducer,
   planetsState: PlanetsReducer,
+  starshipsState: StartShipsReducer,
 });
 
 export default rootReducer;
