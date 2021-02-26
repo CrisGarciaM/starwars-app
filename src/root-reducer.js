3a59f331412251b2e25755fbed999926721af8bf
CristoferGarcia1984 <60959942+CristoferGarcia1984@users.noreply.github.com>
2021-02-26 11:06:06 +0100

    Add planetsState to combined reducers

--- src/root-reducer.js
+++ src/root-reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import CharactersReducer from './redux/characters/characters-reducer';
+import PlanetsReducer from './redux/planets/planets-reducer';
 
 const rootReducer = combineReducers({
   charactersState: CharactersReducer,
+  planetsState: PlanetsReducer,
 });
 
 export default rootReducer;
