7d73de64d2ed804662a80d21f37d69c8677da355
CristoferGarcia1984 <60959942+CristoferGarcia1984@users.noreply.github.com>
2021-03-01 13:09:25 +0100

    Add Header component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import OptionsInput from './components/OptionsInput/OptionsInput';
 import StarShips from './components/Starships/Startships';
 import './App.scss';
 import Species from './components/Species/Species';
+import Header from './components/Header/Header';
 
 const App = () => {
   const [search, setSearch] = useState('');
@@ -15,18 +16,34 @@ const App = () => {
 
   return (
     <div className="main-app">
+      <div className="main-header">
+        <Header
+          title={
+            <a href="https://fontmeme.com/fonts/star-jedi-font/">
+              <img
+                src="https://fontmeme.com/permalink/210301/6c36c878e49730cfb15eb89d81a355a9.png"
+                alt="star-jedi-font"
+                border="0"
+                className="header-img"
+              ></img>
+            </a>
+          }
+        />
+      </div>
       <OptionsInput value={search} onChange={handleChange} />
-      {search === 'planets' ? (
-        <Planets />
-      ) : search === 'people' ? (
-        <Characters />
-      ) : search === 'starships' ? (
-        <StarShips />
-      ) : search === 'species' ? (
-        <Species />
-      ) : (
-        <Characters />
-      )}
+      <div className="cards-container">
+        {search === 'planets' ? (
+          <Planets />
+        ) : search === 'people' ? (
+          <Characters />
+        ) : search === 'starships' ? (
+          <StarShips />
+        ) : search === 'species' ? (
+          <Species />
+        ) : (
+          <Characters />
+        )}
+      </div>
     </div>
   );
 };
