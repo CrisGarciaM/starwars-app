942ef6ca7fc27004ab289ac5f599bf4884d95061
CristoferGarcia1984 <60959942+CristoferGarcia1984@users.noreply.github.com>
2021-03-01 09:59:23 +0100

    Add species reducer

--- src/root-reducer.js
+++ src/root-reducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import CharactersReducer from './redux/characters/characters-reducer';
 import PlanetsReducer from './redux/planets/planets-reducer';
 import StartShipsReducer from './redux/starships/starships-reducer';
+import SpeciesReducer from './redux/species/species-reducer';
 
 const rootReducer = combineReducers({
   charactersState: CharactersReducer,
   planetsState: PlanetsReducer,
   starshipsState: StartShipsReducer,
+  speciesState: SpeciesReducer,
 });
 
 export default rootReducer;
