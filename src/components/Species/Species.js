0aed9b256a451401bc9ed2687d53563a6ef4a653
CristoferGarcia1984 <60959942+CristoferGarcia1984@users.noreply.github.com>
2021-03-01 09:58:37 +0100

    Create initial species rendering

---
+++ src/components/Species/Species.js
@@ -0,0 +1,62 @@
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import ParentContainer from '../ParentContainer/ParentContainer';
+import SpeciesCard from './SpeciesCard/SpeciesCard';
+import { fetchSpecies } from '../../redux/species/species-actions';
+import { mainEndPoints } from '../../Api/endpoints';
+import PaginationContainer from '../Pagination/Pagination';
+import '../../styles/commonStyles.scss';
+
+const Species = (props) => {
+  const { fetchSpecies, speciesData } = props;
+  const { species, loading } = speciesData;
+
+  useEffect(() => {
+    fetchSpecies(mainEndPoints.species);
+  }, [fetchSpecies]);
+
+  if (loading) {
+    return <div className="w-100 text-center">Loading...</div>;
+  }
+
+  return (
+    <div>
+      <ParentContainer>
+        {species.results.map((singleSpecie) => {
+          return (
+            <SpeciesCard
+              key={singleSpecie.id}
+              id={singleSpecie.id}
+              name={singleSpecie.name}
+              classification={singleSpecie.classification}
+              designation={singleSpecie.designation}
+              averageHeight={singleSpecie.average_height}
+              skinColors={singleSpecie.skin_colors}
+              hairColors={singleSpecie.hair_colors}
+              eyeColors={singleSpecie.eye_colors}
+              averageLifespan={singleSpecie.average_lifespan}
+              language={singleSpecie.language}
+            />
+          );
+        })}
+      </ParentContainer>
+      <div>
+        <PaginationContainer />
+      </div>
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => {
+  return {
+    speciesData: state.speciesState,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    fetchSpecies: (endpoint) => dispatch(fetchSpecies(endpoint)),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Species);
